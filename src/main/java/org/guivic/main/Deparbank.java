352c57357b60709468934e6532752e48f684fd88
Guillem Vicente Juan <guillem.vicente.juan@gmail.com>
2024-11-02 21:15:17 +0100

    user input added

--- src/main/java/org/guivic/main/Deparbank.java
+++ src/main/java/org/guivic/main/Deparbank.java
@@ -9,41 +9,48 @@ import java.util.Scanner;
 public class Deparbank {
 
     public static void main(String[] args) {
-        String iban = "ES6621000418401234567891";
-        String accountHolder = "Chemi";
-        Double balance = 500.0;
+        try {
+            System.out.print("IBAN : ");
+            String iban = new Scanner(System.in).nextLine();
+            System.out.print("Account holder : ");
+            String accountHolder = new Scanner(System.in).nextLine();
+            System.out.print("Balance : ");
+            Double balance = new Scanner(System.in).nextDouble();
 
-        BankAccount bankAccount = new BankAccount(iban, accountHolder, balance);
-        boolean exit = false;
+            BankAccount bankAccount = new BankAccount(iban, accountHolder, balance);
+            boolean exit = false;
 
-        Map<Integer, MenuOption> options = new HashMap<>();
+            Map<Integer, MenuOption> options = new HashMap<>();
 
-        optionsToMap(options);
+            optionsToMap(options);
 
-        while (!exit) {
-            System.out.print("""
-                    MAIN MENU
-                    =========
-                    [ 1 ] Account details
-                    [ 2 ] Show IBAN
-                    [ 3 ] Show Account Holder
-                    [ 4 ] Show Balance
-                    [ 5 ] Deposit
-                    [ 6 ] Withdraw
-                    [ 7 ] Movements
-                    [ 8 ] Exit
-                    ---
-                    Choose an option: \s""");
-            int option = new Scanner(System.in).nextInt();
+            while (!exit) {
+                System.out.print("""
+                        MAIN MENU
+                        =========
+                        [ 1 ] Account details
+                        [ 2 ] Show IBAN
+                        [ 3 ] Show Account Holder
+                        [ 4 ] Show Balance
+                        [ 5 ] Deposit
+                        [ 6 ] Withdraw
+                        [ 7 ] Movements
+                        [ 8 ] Exit
+                        ---
+                        Choose an option: \s""");
+                int option = new Scanner(System.in).nextInt();
 
-            MenuOption menuOption = options.get(option);
-            if (menuOption != null) {
-                menuOption.execute(bankAccount);
-                System.out.print("Press any key to continue...: ");
-                String scan = new Scanner(System.in).nextLine();
-            } else {
-                exit = true;
+                MenuOption menuOption = options.get(option);
+                if (menuOption != null) {
+                    menuOption.execute(bankAccount);
+                    System.out.print("Press any key to continue...: ");
+                    String scan = new Scanner(System.in).nextLine();
+                } else {
+                    exit = true;
+                }
             }
+        } catch (Exception e) {
+            System.out.println("ERROR: Incorrect Input");
         }
     }
 
